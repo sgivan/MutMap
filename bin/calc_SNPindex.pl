969df029880f11e762968bfaafe897286e96c6b0
Scott A. Givan <sgivan@yahoo.com>
2015-12-17 10:03:20 -0600

    SNPindex.pl builds array of arrays correctly.

--- bin/calc_SNPindex.pl
+++ bin/calc_SNPindex.pl
@@ -28,14 +28,16 @@ use autodie;
 use Getopt::Long; # use GetOptions function to for CL args
 use warnings;
 use strict;
+use Data::Dumper;
 use lib '/share/apps/perl5/vcftools/lib/site_perl/5.14.2';
 use Vcf;
 
-my ($debug,$verbose,$help,$infile,$outfile);
+my ($debug,$verbose,$help,$infile,$outfile,$window,$blockcnt);
 
 my $result = GetOptions(
     "infile:s"  =>  \$infile,
     "outfile:s" =>  \$outfile,
+    "window:i"  =>  \$window,
     "debug"     =>  \$debug,
     "verbose"   =>  \$verbose,
     "help"      =>  \$help,
@@ -47,26 +49,61 @@ if ($help) {
 }
 
 $infile = 'infile' unless ($infile);
+$window = 5 unless ($window);
 
 my $vcf = Vcf->new(file=>$infile);
 $vcf->parse_header();
 
-my $cnt = 0;
+my ($cnt,$i,$windowcnt) = (0,0,1);
+my (@SNPblock,@SNPlocalblock) = ();
 while (my $x=$vcf->next_data_array()) {
     my $info_column = $vcf->get_column($x,'INFO');
     my $DP4 = $vcf->get_info_field($info_column,'DP4');
-    #say $x->[1], "\t", $info_column, "\t", $DP4;
     my @read_cnt = split /,/, $DP4;
+   
     my $SNP_index = ($read_cnt[2] + $read_cnt[3])/($read_cnt[0] + $read_cnt[1] + $read_cnt[2] + $read_cnt[3]);
-    #say $x->[1], "\t", $DP4, "\t", $SNP_index;
-    printf "%i\t%s\t%3.2f\n", $x->[1], $DP4, $SNP_index;
+    
+    if ($debug) {
+        say "\nwindow cnt = $windowcnt";
+        say "i = $i";
+        printf "%i\t%s\t%3.2f\n", $x->[1], $DP4, $SNP_index;
+    }
+
+    # build array of windows (array of arrays)
+    if ($i >= $window) {
+        say "pushing SNPlocalblock onto SNPblock" if ($debug);
+        push(@SNPblock,[@SNPlocalblock]);
+        @SNPlocalblock = ();
+        $i = 0;
+        ++$windowcnt;
+        redo;
+    } elsif ($i < $window) {
+        say "adding to SNPlocalblock" if ($debug);
+        push(@SNPlocalblock, [$x->[1], $DP4, $SNP_index]);
+    }
+
+    ++$i;
     last if ($debug && ++$cnt >= 10);
+}
+if (@SNPlocalblock) {
+    push(@SNPblock,[@SNPlocalblock]);
+}
+
+if ($debug) {
+    print Dumper(@SNPblock);
+
 }
 
 sub help {
 
-    say <<HELP;
+say <<HELP;
 
+    "infile:s"  =>  \$infile,
+    "outfile:s" =>  \$outfile,
+    "window:i"  =>  \$window,
+    "debug"     =>  \$debug,
+    "verbose"   =>  \$verbose,
+    "help"      =>  \$help,
 
 HELP
 
