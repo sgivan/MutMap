eff120df2d44723f62f17365687598956ffabb33
Scott A. Givan <sgivan@yahoo.com>
2016-02-03 22:31:58 -0600

    Deleted bock of commented lines in bsub_loop.sh

--- bin/bsub_loop.sh
+++ bin/bsub_loop.sh
@@ -41,20 +41,6 @@ do
     stop=$(echo $dir | cut -f 2 -d '.' | cut -f 2 -d '-')
     #echo "start: $start"
     #echo "stop: $stop"
-    # bsm -J ${start}.${stop} '~/projects/mutmap/bin/calc_SNPindex+.pl --ignore ../B2SNPindex_w1_gff_masked_overlap_AD.1.txt --maskedseq \
-    # ../../masked_seqs/ForrestConcat.fa.masked --homoinfile $dir --hetinfile ../het.bcf.gz --refseqfile \
-    # ../../index/ForrestConcat.fa --tab --gff ../Gmax_cds.blastn.gff --mincoord --increment 500000 --window 8000000 --refseqname concatseq \
-    # --start $start --stop $stop  > ${dir}.SNPindex'
-
-    #bsub -u givans@missouri.edu -B -N -R 'span[hosts=1]' -o %J.o -e %J.e -J ${dir}.${start}-${stop} "~/projects/mutmap/bin/calc_SNPindex+.pl \
-    #--ignore ../B2SNPindex_w1_gff_masked_overlap_AD.1.txt --maskedseq ../../masked_seqs/ForrestConcat.fa.masked --homoinfile $dir \
-    #--hetinfile ../het.bcf.gz --refseqfile ../../index/ForrestConcat.fa --tab --gff ../Gmax_cds.blastn.gff \
-    #--mincoord --increment 50000 --window 1000000 --refseqname concatseq --start $start --stop $stop  > ${dir}.SNPindex"
-
-    #bsub -u givans@missouri.edu -B -N -R 'span[hosts=1]' -o %J.o -e %J.e -J ${dir}.${start}-${stop} "~/projects/mutmap/bin/calc_SNPindex+.pl \
-    #--ignore B2SNPindex_w1_gff_masked_overlap_AD.1.txt --maskedseq ForrestConcat.fa.masked --homoinfile $dir \
-    #--hetinfile het.bcf.gz --refseqfile ForrestConcat.fa --tab --gff Gmax_cds.blastn.gff \
-    #--mincoord --increment 50000 --window 500000 --refseqname concatseq --start $start --stop $stop  > ${dir}.SNPindex"
     
     bsub -u givans@missouri.edu -B -N -R 'span[hosts=1] rusage[mem=10000]' -o %J.o -e %J.e -J ${dir}.${start}-${stop} "~/projects/mutmap/bin/calc_SNPindex+.pl \
     --ignore B2SNPindex_w1_gff_masked_overlap_AD.1.txt --maskedseq ForrestConcat.fa.masked --homoinfile $dir \
